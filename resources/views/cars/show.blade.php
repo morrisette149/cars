ffd58d1fa134a095107f48ae002268a5564467b1
morrisette <morrisetteluncamimie@gmail.com>
2023-04-06 23:43:37 +0800

    commit

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Show List of Cars') }}</div>
+                <div class="card-header">{{ __('Show of Cars') }}</div>
 
                 <div class="card-body">
                   <div class="form-group">
